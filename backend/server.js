4ba807b7f5bc1e2d5f7d4d949194eb4ad16d3043
baito.ai <mrchinese2005@gmail.com>
2026-08-07 10:03:56 +0500

    Add vercel.json

--- backend/server.js
+++ backend/server.js
@@ -60,8 +60,8 @@ res.json({
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
